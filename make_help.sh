2addb43e787bfb8a669e2ecc510b1b33cd7571b5
Little John <amingg840@gmail.com>
2026-03-22 12:04:42 -0400

    Fix indexing error

--- make_help.sh
+++ make_help.sh
@@ -3,7 +3,7 @@ ls *.c | sed G | grep func >> files.c
 # ^ get all the test files
 FILECOUNT=$(wc -l < files.c)
 # and how many there are
-for ((i=0; i<=FILECOUNT-1; i++)); do
+for ((i=1; i<=FILECOUNT; i++)); do
     FILE=$(awk -v i="$i" 'NR == i' files.c)
     # get names one at a time and compile them
     ($CC $FLAGS $DEBUG "$FILE" -o "$(basename -s .c "$FILE").exe")
